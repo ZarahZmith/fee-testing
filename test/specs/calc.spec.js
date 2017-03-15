91291452442da65a432d2265436dfd0f80570609
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-15 15:10:59 -0400

    Ran test to ensure function worked when the argument contained multiple arguments in an array

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -6,13 +6,22 @@
     // TODO: Your tests go here!
     describe('calc', function() {
       describe('sum function', function() {
+        
         it('should return the sum of one plus one', function() {
           let result = window.calc.sum([1, 1]);
 
           expect( result ).to.be.a('number');
           expect( result ).to.equal(2);
-
         });
+
+        it('should give the sum when provided and array of multiple numbers', function() {
+          let result = window.calc.sum([4, 7, 13]);
+
+          expect( result ).to.be.a('number');
+          expect( result ).to.equal(24);
+        });
+
+
       });
     });
 
