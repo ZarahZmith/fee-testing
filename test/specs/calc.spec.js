42db7de9ec50867e0214bfcc0e3439c6a258531c
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-15 15:05:19 -0400

    Set up simple test to ensure tests were running correctly

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -1,10 +1,23 @@
 (function() {
     'use strict';
 
+    let expect = chai.expect;
 
     // TODO: Your tests go here!
+    describe('calc', function() {
+      describe('sum function', function() {
+        it('should return the sum of one plus one', function() {
+          let result = window.calc.sum([1, 1]);
 
-    
+          expect( result ).to.be.a('number');
+          expect( result ).to.equal(2);
+
+        });
+      });
+    });
+
+
+
 
 
 })();
